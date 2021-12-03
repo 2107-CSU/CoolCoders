6063a47eeb72011ce8eb5e255880b48586f8eb38
Rafa Brown <shane.rafael@zohomail.com>
2021-12-02 19:30:58 -0700

    Add mock data for users, products, categories

---
+++ db/seedData.js
@@ -0,0 +1,75 @@
+const mockUsers = [
+  {
+    email: "alienwear0881@gmail.com",
+    name: "Allen Wear",
+    password: "ufosRreal!!",
+  },
+  {
+    email: "ellojojo265@outlook.com",
+    name: "Elliott Jones",
+    password: "eieio34",
+  },
+  {
+    email: "lilgranny25@aol.com",
+    name: "Myrtle Castles",
+    password: "ilovemygrandson1234",
+  },
+  {
+    email: "lademigod@gmail.com",
+    name: "Percy Jackson",
+    password: "zeusStan27",
+  },
+  {
+    email: "cowboyhunter73@yahoo.com",
+    name: "John Marston",
+    password: "rdr2isbetter",
+  },
+];
+
+const mockProducts = [
+  {
+    title: "Greatsword",
+    description: "A really great sword.",
+    price: 1000,
+    quantity: 100,
+    categoryId: 0,
+  },
+  {
+    title: "Power Armor",
+    description: "It makes you feel, like, really powerful, dude.",
+    price: 1000000,
+    quantity: 1,
+    categoryId: 0,
+  },
+  {
+    title: "",
+    description: "",
+    price: 0,
+    quantity: 0,
+    categoryId: 0,
+  },
+];
+
+const mockCategories = [
+  {
+    name: "weapons",
+  },
+  {
+    name: "powerups",
+  },
+  {
+    name: "healing items",
+  },
+  {
+    name: "armor",
+  },
+  {
+    name: "magic",
+  },
+  {
+    name: "potions",
+  },
+  {
+    name: "talismans",
+  },
+];
