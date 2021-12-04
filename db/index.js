3fb976674b34fa4a098c61ea126b7c0c47b0e01c
jennymarie1027 <jennymarie1027@gmail.com>
2021-12-04 12:27:30 -0700

    moved client & product db adapters into their own files

--- db/index.js
+++ db/index.js
@@ -1,13 +1,9 @@
 // Connect to DB
-const { Client } = require('pg');
-const DB_NAME = 'change-this-name'
-const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
-const client = new Client(DB_URL);
 
 // database methods
 
-// export
+const productDbAdapters = require('./products');
+
 module.exports = {
-  client,
-  // db methods
+  ...productDbAdapters,
 }
