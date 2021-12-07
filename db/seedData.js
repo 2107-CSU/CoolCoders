a3bea3b32ad5a877890e76f5c3f270b312477c71
Brandon <blor15@hotmail.com>
2021-12-06 22:10:08 -0700

    Finished the functions for creating the initial data from mockData.js

--- db/seedData.js
+++ db/seedData.js
@@ -103,17 +103,93 @@ async function createTables() {
 BUILDING FUNCTIONS USING MOCK DATA FROM mockData.js
 */
 
-async function createMockUser() {
-    console.log('Starting to create mock users...')
-    try{
-        
-    }
-}
+async function createInitialUsers() {
+   console.log('Starting to create mock users...');
+   try { 
+       const users = await Promise.all(mockUsers.map(createUser));
+
+       console.log('Users created:');
+       console.log(users);
+       console.log('Finished creating users!');
+   }   catch (error) {
+       console.error('Error ceating users!');
+   }   throw error;
+};
+
+async function createInitialProducts() {
+    try {
+        console.log('Starting to create products...');
+
+        const products = await Promise.all(mockProducts.map(createProduct));
+
+        console.log('Products created:');
+        console.log(products);
+
+        console.log('Finished creating products!');
+    }   catch (error) {
+        console.error('Error creating products!');
+        throw error;
+    }   
+};
+
+async function createInitialCategories() {
+    try {
+        console.log('Starting to create categories...');
+
+        const categories = await Promise.all(mockCategories.map(createCategories));
+
+        console.log('Categories created:');
+        console.log(categories);
+
+        console.log('Finished creating categories!');
+    }   catch (error) {
+        console.error('Error creating categories!');
+        throw error;
+    }   
+};
+
+async function createInitialOrders() {
+    try {
+        console.log('Starting to create orders...');
+
+        const orders = await Promise.all(mockOrders.map(createOrders));
+
+        console.log('Orders created:');
+        console.log(orders);
+
+        console.log('Finished creating orders!');
+    }   catch (error) {
+        console.error('Error creating orders!');
+        throw error;
+    }   
+};
+
+async function createInitialReviews() {
+    try {
+        console.log('Starting to create reviews...');
+
+        const reviews = await Promise.all(mockReviews.map(createReviews));
+
+        console.log('Reviews created:');
+        console.log(reviews);
+
+        console.log('Finished creating reviews!');
+    }   catch (error) {
+        console.error('Error creating reviews!');
+        throw error;
+    }   
+};
+
 
 async function rebuildDB() {
     try {
         await dropTables();
         await createTables()
+        await createInitialUsers();
+        await createInitialProducts();
+        await createInitialCategories();
+        await createInitialOrders();
+        await createInitialReviews();
     }   catch (error) {
         console.log('Error during rebuildDB');
         throw (error);
