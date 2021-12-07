e3e67fda2babbce33039e0aa16d5d07d38915a5a
RicardoMartinez.Denver@gmail.com <RicardoMartinez.Denver@gmail.com>
2021-12-07 07:15:35 -0700

    fix users table definition

--- db/seedData.js
+++ db/seedData.js
@@ -43,6 +43,14 @@ async function createTables() {
                 'user',
                 'guest'
             );
+            CREATE TABLE users(
+                id SERIAL PRIMARY KEY,
+                email VARCHAR(255) UNIQUE NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                active BOOLEAN DEFAULT true,
+                userStatus user_type NOT NULL
+            );
 
 
         `);
