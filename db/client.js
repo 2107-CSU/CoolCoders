86ad0ab8d0c6dc26384a0afaac65ece0c3461682
RicardoMartinez.Denver@gmail.com <RicardoMartinez.Denver@gmail.com>
2021-12-07 20:35:56 -0700

    db/client.js

--- db/client.js
+++ db/client.js
@@ -3,6 +3,5 @@ const DB_NAME = 'capstone-dev'
 const DB_URL = process.env.DATABASE_URL || `postgres://localhost:5432/${ DB_NAME }`;
 
 const { Client } = require('pg');
-const client = new Client(DB_URL);
 
 module.exports = client;
