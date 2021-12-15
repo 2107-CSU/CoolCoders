2bfc640a71cf5ba10182b3ac286266f371c64b88
Brandon <blor15@hotmail.com>
2021-12-14 19:30:13 -0700

    add image onto the front end for products

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from "react"
 import { getProducts } from "../api";
 // import { getSingleProduct } from "../api";
 
-import {Link} from 'react-router-dom';
+import { Route, Link} from 'react-router-dom';
+import SingleProduct from "./SingleProduct";
 
 const Products = () => {
    
@@ -32,15 +33,20 @@ const Products = () => {
                 ? (
                     <ul>
                         {products.map(product => {
-                            return <div className="border mb-4 roundeed overflow-hidden">
-                                    <h2 clasName="p-3" key={product.id}>{product.title}</h2>
-                                    <p className="mb-3">Description: {product.description}</p>
-                                    <p className="font-bold mb-3">Price: ${product.price}</p>
-                                    <p>Quantity: {product.quantity}</p>
-                                    <Link to={`/products/${product.id}`}>
-                                        View
-                                    </Link>
+                            return (
+                            <div className="border mb-4 roundeed overflow-hidden">
+                                <h2 className="p-3" key={product.id}>{product.title}</h2>
+                                <div>
+                                    <img src={product.photo}/>
                                 </div>
+                                <p className="mb-3">Description: {product.description}</p>
+                                <div className="font-bold mb-3">Price: ${product.price}</div>
+                                <div>Quantity: {product.quantity}</div>
+                                <Link  to={`/products/${product.id}`}>
+                                    View
+                                </Link>
+                            </div>
+                            )
                         })}
                     </ul>
                 )  : <h3>Loading</h3>}
