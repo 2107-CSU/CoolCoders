da8744a04dea5d844cfe68f0966e3ce278e33712
Brandon <blor15@hotmail.com>
2021-12-14 19:31:47 -0700

    add route path for product id

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route } from 'react-router-dom';
 
 import Cart from './Cart'
 import Products from './Products';
+import SingleProduct from './SingleProduct';
 import Header from './Header';
 import Homepage from './Homepage'
 import Login from './Login';
@@ -26,6 +27,7 @@ const App = () => {
       <Route path='/login' exact render={(routeProps) => <Login {...routeProps} setToken={setToken} token={token}/> } />
       <Route path='/register' exact render={(routeProps) => <Login {...routeProps} setToken={setToken} /> } />      
       <Route path='/products' exact render={() => <Products /> }/>
+      <Route path='/products/:id' exact render={() => <SingleProduct />}/>
       <Route path='/cart' exact render={() => <Cart /> } />
       <Route path='/' exact render={() => <Homepage /> } />
     </BrowserRouter>
