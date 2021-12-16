790fee6fcd2a427b7d93218742822924939e617e
jennymarie1027 <jennymarie1027@gmail.com>
2021-12-16 12:45:50 -0700

    sign the token with the userStatus, grab it from token at jwt.verify

--- routes/index.js
+++ routes/index.js
@@ -28,12 +28,15 @@ apiRouter.use(async (req, res, next) => {
 
       try {
           //attempt to verify token and destructure the id from the return data
-          const {id} = jwt.verify(token, JWT_SECRET);
+          const {id, userStatus} = jwt.verify(token, JWT_SECRET);
 
           if (id) {
               //if a valid user is returned, add it to the request object
               req.user = await getUserById(id);
-
+              // if userStatus = admin, add a key to the req.user object
+              if (userStatus === 'admin') {
+                req.user.admin = true;
+              }
               //then move to the next middleware
               next();
           }
