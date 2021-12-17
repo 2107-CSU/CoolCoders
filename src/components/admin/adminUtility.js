355d3bc8cc454ab515cde7dc29737d302b5e4b54
jennymarie1027 <jennymarie1027@gmail.com>
2021-12-17 10:10:24 -0700

    cleaned up console.logs inside adminUtility.js

--- src/components/admin/adminUtility.js
+++ src/components/admin/adminUtility.js
@@ -1,51 +1,66 @@
 async function handleNewProduct(title, description, price, qty, categoryId, photo, token){
-    const res = await fetch(`http://localhost:2345/api/products`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        },
-        body: JSON.stringify({
-            title: title,
-            description: description,
-            price: price,
-            quantity: qty,
-            categoryId: categoryId,
-            photo: photo
+    try {
+        const res = await fetch(`http://localhost:2345/api/products`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                title: title,
+                description: description,
+                price: price,
+                quantity: qty,
+                categoryId: categoryId,
+                photo: photo
+            })
         })
-    })
-    const data = await res.json();
-    console.log('data from handleNewProduct = ', data)
+        const data = await res.json();
+        console.log('data from handleNewProduct = ', data)
+    } catch (error) {
+        console.log(error)
+        throw error;
+    }
 }
 
 async function handleUpdateProduct(productId, {title, description, price, qty, categoryId, photo}, token){
-    const res = await fetch(`http://localhost:2345/api/products/${productId}`, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        },
-        body: JSON.stringify({
-            title,
-            description,
-            price,
-            quantity: qty,
-            categoryId,
-            photo
+    try {
+        const res = await fetch(`http://localhost:2345/api/products/${productId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                price,
+                quantity: qty,
+                categoryId,
+                photo
+            })
         })
-    })
-    await res.json();
+        await res.json();
+    } catch (error) {
+        console.log(error)
+        throw error;
+    }
 }
 
 async function handleDeleteProduct(productId, token){
-    const res = await fetch(`http://localhost:2345/api/products/${productId}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    await res.json();
+    try {
+        const res = await fetch(`http://localhost:2345/api/products/${productId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        })
+        await res.json();
+    } catch (error) {
+        console.log(error)
+        throw error;
+    }
 }
 
 const getProductById = async (productId, setSelectedProduct) => {
