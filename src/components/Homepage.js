8fd80909a884d581362a42b59de30148945ac125
RicardoMartinez.Denver@gmail.com <RicardoMartinez.Denver@gmail.com>
2021-12-29 20:26:10 -0700

    layout for homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Homepage = () => {
     return (
-        <div id="homepage" className='marginTop'>
-         Welcome to the homepage 😊
+        <div id="homepage">
+            <div id="center-container" className='center'>
+                <div id="banner">Banner</div>
+                <div id="featured-container">Featured Products</div>
+            </div>
         </div>
     )
 }
