f456689937c9d65d738a377006380714a7e2665e
Rafa Brown <shane.rafael@zohomail.com>
2022-01-03 12:34:21 -0700

    fix useEffect import on ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { useEffect } from 'react/cjs/react.development';
+import React, { useEffect } from "react";
+import { Route, Redirect } from "react-router-dom";
 
-const ProtectedRoute = ({  user: user, token: token, component: Component, ...rest }) => {
+const ProtectedRoute = ({
+  user: user,
+  token: token,
+  component: Component,
+  ...rest
+}) => {
+  useEffect(() => {
+    console.log(user);
+  }, []);
 
-    useEffect(() => {
-        console.log(user);
-    }, []);
-
-    return (
+  return (
     <Route
       {...rest}
       render={(routeProps) => {
-        if (user && user.userStatus === 'admin') {
-          console.log('following user allowed access = ', user);
-          console.log('user.userStatus === ', user.userStatus)
-          return  <Component token={token} user={user} {...routeProps}/>
+        if (user && user.userStatus === "admin") {
+          console.log("following user allowed access = ", user);
+          console.log("user.userStatus === ", user.userStatus);
+          return <Component token={token} user={user} {...routeProps} />;
         } else {
-          return <Redirect to={{ pathname: '/', state: { from: routeProps.location } }} />
+          return (
+            <Redirect
+              to={{ pathname: "/", state: { from: routeProps.location } }}
+            />
+          );
         }
-      }
-    } />
-  )
-}
+      }}
+    />
+  );
+};
 
 export default ProtectedRoute;
